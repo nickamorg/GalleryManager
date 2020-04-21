6b99b4de3ee349c091d5437553a35df3f7f5a139
Snick <nickamorg0@gmail.com>
2020-04-21 15:38:55 +0300

    Initial screen to load folder path

---
+++ main.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow} = require('electron')
+const url = require('url')
+const path = require('path')
+
+let win
+
+function createWindow() {
+   win = new BrowserWindow({width: 1920, height: 1080})
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }))
+   win.maximize();
+   // win.setMenu(null);
+}
+
+app.on('ready', createWindow)
